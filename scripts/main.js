b1cc4966bbf5ef4ef95078a0eee3d440a62e58f3
unknown <kylebennett9514@gmail.com>
2022-10-07 02:53:25 -0500

    linked pictures from the apis, added buttons, and scoreboard area

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,23 @@
 import { fetchCat } from "./voteData.js";
 import { fetchDog } from "./voteData.js";
+import { getCat } from "./voteData.js";
+import { getDog } from "./voteData.js";
+
+
+
+const renderCatToDOM = () => {
+    let html = `
+    <article>
+        <section class="card">
+        <img src="${getCat()[0].url}" alt="image" class="image">
+    </section>
+    </article>`;
+document.getElementById("cat").innerHTML = html
+}
 
 const displayCat = async () => {
     const catData = await fetchCat()
     renderCatToDOM(catData)
-}
-
-const renderCatToDOM = (catData) => {
-    let html = `
-    <article>
-        <section class="card">
-        <img src="${catData.primaryImageSmall}" alt="image">
-    </section>
-    </article>`;
-document.getElementById("cat").innerHTML = html
 }
 
 const displayDog = async () => {
@@ -21,11 +25,11 @@ const displayDog = async () => {
     renderDogToDOM(dogData)
 }
 
-const renderDogToDOM = (dogData) => {
+const renderDogToDOM = () => {
     let html = `
     <article>
         <section class="card">
-        <img src="${dogData.primaryImageSmall}" alt="image">
+        <img src="${getDog()[0].url}" alt="image" class="image">
     </section>
     </article>`;
 document.getElementById("dog").innerHTML = html
--- scripts/voteData.js
+++ scripts/voteData.js
@@ -1,14 +1,29 @@
 const catAPI = "https://api.thecatapi.com/v1/images/search"
 const dogAPI = "https://api.thedogapi.com/v1/images/search"
 
+const applicationState = {
+    dogs: [],
+    cats: []
+}
+
+
+// fetch commands go to the API 
 export const fetchCat = async () => {
     const dataFetch = await fetch(`${catAPI}`)
     const jsonCat = await dataFetch.json()
-    return jsonCat
+    applicationState.cats = jsonCat;
 }
 
 export const fetchDog = async () => {
     const dataFetch = await fetch(`${dogAPI}`)
     const jsonDog = await dataFetch.json()
-    return jsonDog
+    applicationState.dogs = jsonDog;
+}
+
+export const getCat = () => {
+    return applicationState.cats.map((x) => ({ ...x }));
+}
+
+export const getDog = () => {
+    return applicationState.dogs.map((x) => ({ ...x }));
 }
